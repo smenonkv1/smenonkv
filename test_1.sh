cfa840e717c3b9efbe34608ef79a167815e9bfb6
smenonkv <kvsmenon23@gmail.com>
2021-05-12 11:24:51 +0000

    fourth commit

---
+++ test_1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo 'Enter the score'
+
+read x
+
+if [[ $x -gt 90 ]];
+then
+  echo “You have won the First Prize”
+
+elif [[ $x -ge 60 ]];
+then
+  echo “You have won the Second Prize”
+
+elif [[ $x -ge 30 ]];
+then
+  echo “You have won the third Prize”
+else
+  echo “You have failed!”
+fi
