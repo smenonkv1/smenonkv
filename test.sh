ec2479949695425536d09b616820f5ba0223c7cf
smenonkv <kvsmenon23@gmail.com>
2021-05-12 11:19:20 +0000

    first commit

---
+++ test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo 'Enter the score'
+
+read x
+
+if [[ $x -ge 90 ]];
+then
+  echo “You have won the First Prize”
+
+elif [[ $x -ge 60 ]];
+then
+  echo “You have won the Second Prize”
+
+elif [[ $x -ge 30 ]];
+then 
+  echo “You have won the third Prize”
+else
+  echo “Please try again”
+fi
